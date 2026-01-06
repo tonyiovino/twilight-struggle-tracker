b8bb34cdc643b86863737c749861f601eda6dd55
tonyiovino <antonioiovino2004tony@gmail.com>
2026-01-06 19:45:09 +0100

    json to const

---
+++ tst-app/store/regions_data.ts
@@ -0,0 +1,50 @@
+export const regions = {
+  Europe: {
+    name: 'Europe',
+    points: {
+      presence: 3,
+      domination: 7,
+      control: 1000,
+    },
+  },
+  Asia: {
+    name: 'Asia',
+    points: {
+      presence: 3,
+      domination: 7,
+      control: 9,
+    },
+  },
+  CA: {
+    name: 'Central America',
+    points: {
+      presence: 1,
+      domination: 3,
+      control: 5,
+    },
+  },
+  SA: {
+    name: 'South America',
+    points: {
+      presence: 2,
+      domination: 5,
+      control: 6,
+    },
+  },
+  Africa: {
+    name: 'Africa',
+    points: {
+      presence: 1,
+      domination: 4,
+      control: 6,
+    },
+  },
+  ME: {
+    name: 'Middle East',
+    points: {
+      presence: 3,
+      domination: 5,
+      control: 7,
+    },
+  },
+};
